035cf74e1bf496b1770d79369b9257c2523a77b6
Disquse <disquse@mail.com>
2019-03-11 03:15:51 +0300

    Remove deprecated codebase

--- pages/loadfeedbacks.php
+++
@@ -1,9 +0,0 @@
-<?php
-	if ( $_GET['password'] == 'pass' ) {
-		$text = file_get_contents("feedbacks.json"); $text = '['.$text.']'; $text = str_replace('}{', '},{', $text);
-		$feedbacks = json_decode($text, true);
-		echo '<pre>'; print_r($feedbacks); echo '</pre>';
-	} else {
-		echo '<h1>NO ACCESS!</h1><hr>';
-	}
-?>
--- pages/sendfeedback.php
+++
@@ -1,5 +0,0 @@
-<?php
-	$feedback['Time'] = mktime();
-	$feedback['Data'] = $_POST['data'];
-	file_put_contents('feedbacks.json', json_encode($feedback), FILE_APPEND);
-?>
